e56789023ea7b7b238c2245d7e6d79a1ef3fd7c6
Potato <poltoradnevone@gmail.com>
2022-06-02 20:52:19 +0400

    uteoat

--- public/js/agario/agario.js
+++ public/js/agario/agario.js
@@ -44,7 +44,7 @@ button.addEventListener('click', async () => {
 
   startDiv.style.display = 'none';
 
-  setInterval(main, 30);
+  setInterval(main, 50);
 });
 
 socket.on('playerData', (data) => {
@@ -54,7 +54,7 @@ socket.on('playerData', (data) => {
 socket.on('Players', (data) => {
   otherPlayers = [];
   data.forEach((data) => {
-    if (data.id != player.id) {
+    if (data.id !== player.id) {
       let newPlayer = new Player(
         data.pos.x,
         data.pos.y,
@@ -102,7 +102,7 @@ function main() {
   ctx.stroke();
   ctx.closePath();
 
-  for (blob of blobs) {
+  blobs.forEach((blob) => {
     if (
       blob.inWindow(
         player.pos.x,
@@ -110,14 +110,11 @@ function main() {
         canvas.width * (player.r / player.originalR),
         canvas.height * (player.r / player.originalR)
       )
-    ) {
+    )
       blob.show(ctx);
-    }
-  }
+  });
 
-  for (oplayer of otherPlayers) {
-    oplayer.show(ctx);
-  }
+  otherPlayers.forEach((player) => player.show(ctx));
 
   player.show(ctx);
   ctx.textAlign = 'start';
